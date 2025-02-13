49ae83e51c027d596b135afe139d49c9bdda6254
rojass2001 <rojascherian@gmail.com>
2025-02-13 13:08:52 +0530

    first commit

--- src/Components/Recipes/Reciepes.jsx
+++ src/Components/Recipes/Reciepes.jsx
@@ -40,8 +40,8 @@ function Reciepes() {
             <p className='font-bold'>{a.price}</p>
             
             </div>
-            <div className='w-full justify-center place-items-center'>
-            <div onClick={()=>dispatch(addtocart(a))}className=' w-16 h-8 place-content-center  place-items-center border-2 border-green-500 
+            <div className='w-full text-center justify-center place-items-center'>
+            <div onClick={()=>dispatch(addtocart(a))}className=' w-16 h-8 place-content-center text-center  place-items-center border-2 border-green-500 
             rounded-lg'><FaCartShopping className='text-xl'/>
             </div>
             </div>
