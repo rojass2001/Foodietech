6e73285596d116b48d32e29b4e6a172f45218237
rojass2001 <rojascherian@gmail.com>
2025-02-14 19:31:50 +0530

    first commit

--- src/Components/Home/Service/Servicearray.jsx
+++ src/Components/Home/Service/Servicearray.jsx
@@ -2,31 +2,31 @@
 import { FaShippingFast,FaHeadset,FaLock,FaTag,FaMoneyBillWave } from "react-icons/fa"
 export const servicearray=[
    {
-    icon:<FaShippingFast className="text-3xl text-center "/>,
+    icon:<FaShippingFast className="text-4xl mx-auto"/>,
     title:"Free Shipping",
     des:"Get your orders delivered with no extra cost",
     delay:"100"
    },
    {
-    icon:<FaHeadset className="text-3xl text-center"/>,
+    icon:<FaHeadset className="text-4xl mx-auto "/>,
     title:"Support 24/7",
      des:"We are higher assist you anytime",
       delay:"300"
    },
    {
-    icon:<FaMoneyBillWave className="text-3xl text-center "/>,
+    icon:<FaMoneyBillWave className="text-4xl mx-auto "/>,
     title:"100% Money Back",
      des:"Full refund if you are not intrested",
      delay:"500"
    },
    {
-    icon:<FaLock className="text-2xl text-center"/>,
+    icon:<FaLock className="text-3xl mx-auto"/>,
     title:"Payment Secure",
      des:"Your payment information safe with us",
       delay:"700"
    },
    {
-    icon:<FaTag className="text-2xl text-center"/>,
+    icon:<FaTag className="text-3xl mx-auto"/>,
     title:"Discount",
      des:"Enjoy the best prices on our products",
       delay:"900"
