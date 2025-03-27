d791320cbbcebe2be3dce21cf2665f53920d5656
rojass2001 <rojascherian@gmail.com>
2025-03-27 14:23:18 +0530

    third commit

--- src/Components/Navbar/Navbar.jsx
+++ src/Components/Navbar/Navbar.jsx
@@ -23,7 +23,7 @@ function Navbar() {
         <Link to="/"><li>Home</li></Link>
         <Link to="/reciepe"><li>Recipes</li></Link>
         <Link to="/login"><li>Login</li></Link>
-        <li>Logout</li>
+        <li className='cursor-pointer'>Logout</li>
       </ul>
 
       <div className='hidden md:flex w-[200px] '><Searchbar/></div>
