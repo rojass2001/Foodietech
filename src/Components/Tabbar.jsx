d11fcd779317aa7b9346dfd995db7100828e863b
rojass2001 <rojascherian@gmail.com>
2025-04-19 20:46:11 +0530

    second commit

--- src/Components/Tabbar.jsx
+++ src/Components/Tabbar.jsx
@@ -8,8 +8,8 @@ import useAuth from "./customhooks/useAuth";
 function Tabbar() {
  const{logout}=useAuth()
   return (
-    <>
-  <div className="w-full  p-1 flex items-center place-content-center text-green-600 h-[70px] pt-2 fixed z-50 bottom-0 right-0 left-0  md:hidden">
+    
+  <div className="w-full  p-1 flex items-center place-content-center text-green-600 h-[70px] pt-2 fixed z-30 bottom-0 right-0 left-0  md:hidden">
     <div className='w-[97%]  flex justify-around py-1 rounded-3xl  items-center place-content-center bg-black  ' >
       <Link to="/">
         <button className='flex items-center text-sm place-content-center flex-col'><FaHome className='text-2xl ' />Home</button>
@@ -24,8 +24,8 @@ function Tabbar() {
         <button onClick={logout} className='flex items-center text-sm place-content-center flex-col'><IoSettingsOutline className='text-2xl' />logout</button>
       </Link>
     </div>
-      </div>
-      </>
+  </div>
+  
   )
 }
 
