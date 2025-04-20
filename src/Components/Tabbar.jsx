8dae7c52c785c548ad693de3763f14accf7d3a13
rojass2001 <rojascherian@gmail.com>
2025-04-20 15:57:17 +0530

    second commit

--- src/Components/Tabbar.jsx
+++ src/Components/Tabbar.jsx
@@ -9,7 +9,7 @@ function Tabbar() {
   
  const{logout}=useAuth()
   return (
-        <div className="w-full pr-safe-right pt-safe-top pb-safe-bottom pl-safe-left md:hidden p-1 flex items-center place-content-center text-green-600 h-[70px]  fixed z-50 bottom-0 right-0 left-0  ">
+        <div className="w-full pt-2 md:hidden pb-[env(safe-area-inset-bottom)] flex items-center place-content-center text-green-600 h-[70px]  sticky z-50 bottom-0">
           <div className='w-[97%]  flex justify-around py-1 rounded-3xl  items-center place-content-center bg-black  ' >
             <Link to="/">
               <button className='flex items-center text-sm place-content-center flex-col'><FaHome className='text-2xl ' />Home</button>
